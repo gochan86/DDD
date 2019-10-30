1d4229809692edfffe0960fa6b098d9169512425
David Gonzalez <david.gonzalez@iberoservice.com>
2019-10-30 16:17:08 +0100

    falta timeout

--- Project.Application/Modules/Bargains/QueryHandlers/BargainsQueryHandler.cs
+++ Project.Application/Modules/Bargains/QueryHandlers/BargainsQueryHandler.cs
@@ -3,7 +3,13 @@ using Project.Application.Dto.Bargains.Query;
 using Project.Application.Dto.Bargains.QueryResult;
 using Project.Application.DtoConverter.Modules.HotelAggregate;
 using Project.Domain.Connectors;
+using Project.Tools;
+using Project.Tools.Cache;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Project.Application.Modules.Bargains.QueryHandlers
 {
@@ -23,14 +29,44 @@ namespace Project.Application.Modules.Bargains.QueryHandlers
 
         public BargainsQueryResult Retrieve(BargainsQuery query)
         {
+            var response = ApplicationContext.Cache.Get<BargainsQueryResult>("search[" + query.destinationId + "#" + query.nights + "]");
+
+            if (response != null) { return response; }
+
+            response = DoSearch(query);
+
+            if (response.Bargains != null & response.Bargains.Any())
+            {
+                ApplicationContext.Cache.Set("search[" + query.destinationId + "#" + query.nights + "]", response);
+            }
+
+            return response;
+        }
+
+        public BargainsQueryResult DoSearch(BargainsQuery query)
+        {
+
             var hotelList = _hotelConnector.GetHotels(destinationId: query.destinationId, nights: query.nights);
 
-            List<BargainsHotelDto> resultDto = new List<BargainsHotelDto>();
+            List<BargainsHotelDto> resultsDto = new List<BargainsHotelDto>();
 
             if (hotelList != null)
-                resultDto = HotelConverter.Instance.ToDto(hotelList);
+                resultsDto = HotelConverter.Instance.ToDto(hotelList);
 
-            return new BargainsQueryResult() { Bargains = resultDto };
+            var response = new BargainsQueryResult();
+            response.Bargains = resultsDto;
+
+            return response;
+        }
+
+        public void ThrowTask(BargainsQuery query, List<BargainsHotelDto> response)
+        {
+            var tasks = new List<Task>();
+            var connectivityTask = Task.Factory.StartNew(() => DoSearch(query));
+            connectivityTask.ContinueWith(t => { }, TaskContinuationOptions.OnlyOnFaulted);
+            tasks.Add(connectivityTask);
+            Task.WaitAll(tasks.ToArray<Task>(), TimeSpan.FromMilliseconds(int.Parse(ConfigurationManager.AppSettings["SearchTimeOutMilliseconds"])));
+
         }
     }
 }
--- Project.Crosscutting.Bootstrapper/IocRegistration.cs
+++ Project.Crosscutting.Bootstrapper/IocRegistration.cs
@@ -6,6 +6,7 @@ using Project.Data.Connectors.BargainsConnector.Connector;
 using Project.Data.WebApi;
 using Project.Domain.Connectors;
 using Project.Domain.Services;
+using Project.Tools.Cache;
 using Project.Tools.IoC;
 using System;
 using System.Collections.Generic;
@@ -18,19 +19,21 @@ namespace Project.Crosscutting.Bootstrapper
         public static void Register(IIocContainer container)
         {
             container.RegisterInstance<IIocContainer>(container);
-              
+
             // Model services Domain
             container.Register<IHotelConnector, BargainsConnector>();
             container.Register<IProjectService, ProjectService>();
-             
+
             // Command and query 
-            container.Register<IQueryDispatcher, QueryDispatcher>(); 
+            container.Register<IQueryDispatcher, QueryDispatcher>();
             container.RegisterType(typeof(IQueryHandler<BargainsQuery, BargainsQueryResult>), typeof(BargainsQueryHandler));
+
+            // Tools
              
 
             //// Connectors 
             container.Register<IWebApiConnector, WebApiConnector>();
-             
+
         }
     }
 }
